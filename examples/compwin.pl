05c7c553ea7cdffddc6eef724d22ac07f6460fbc
José Joaquín Atria <jjatria@gmail.com>
2019-12-06 17:39:24 +0000

    Tweaks to example scripts

--- examples/compwin.pl
+++ examples/compwin.pl
@@ -1,30 +1,28 @@
-#!/usr/bin/perl
-use warnings; use strict;
+#!/usr/bin/env perl
 
-# compare window functions between 
+use warnings;
+use strict;
+
+# compare window functions between
 # PDL::Audio and PDL::DSP::Windows
 
 use PDL;
 use PDL::Audio qw( gen_fft_window );
 use PDL::DSP::Windows qw( window );
 
-my $N = 5;
+use constant N => 5;
 
-foreach my $name ( ['hamming', 'hamming_ex'], 'hann', 'welch', 'bartlett' ){
-    my ($name_aud, $name_dsp);
-    if (ref($name)) {
-        ($name_aud, $name_dsp) = @$name;
-    }
-    else {
-        $name_aud = $name_dsp  = $name;
-    }
+for ( ['hamming', 'hamming_ex'], ['hann'], ['welch'], ['bartlett'] ) {
+    my ($name_aud, $name_dsp) = @{$_};
+    $name_dsp //= $name_aud;
+
     print "Name: $name_aud $name_dsp\n";
 
-    my $w = gen_fft_window($N,$name_aud);
+    my $w = gen_fft_window( N, $name_aud );
     print 'aud: ';
     print $w->nelem, ': ', $w, "\n";
 
-    $w = window($N,$name_dsp, {per => 1});
+    $w = window( N, $name_dsp, { per => 1 } );
     print 'dsp: ';
     print $w->nelem, ': ', $w, "\n";
 }
